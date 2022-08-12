5b795d9e3e57f1e0dff556be1bdba296f3aabe4d
Gabriel Moreira <gmaestre002@gmail.com>
2022-08-11 23:35:23 -0300

    Initial Commit
    Basic NodeJS Website (express and ejs)

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const ejs = require('ejs');
+
+const api = require('./api/api.json');
+
+const sitePort = 2000;
+
+const app = express();
+
+//Uses
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('public'));  
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
+app.use('/fonts', express.static(__dirname + 'public/fonts'));
+app.use('/scss', express.static(__dirname + 'public/scss'));
+app.use('/html', express.static(__dirname + 'public/html'));
+
+//Views
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, "public/html"));
+
+
+app.get('/', async (req, res) => {
+    res.render('index', { api: api });
+});
+
+app.listen(sitePort, () => console.info(`Rodando: http://127.0.0.1:${sitePort}/`));
